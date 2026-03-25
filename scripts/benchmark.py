d923a1366681d2191d6f19b6e298222c8f40181a
Ulaana <157713810+Ulaana@users.noreply.github.com>
2026-03-25 20:07:59 +0100

    Add files via upload

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,100 +1,75 @@
 from tcxreader.tcxreader import TCXReader
 import numpy as np
 import time
+import json
 
 from tcx_python import bbox, total_distance, elevation_gain, avg_hr, hr_zones, elevation_hr, perf_eff
 from tcx_numpy import bbox_np, total_distance_np, elevation_gain_np, avg_hr_np, hr_zones_np, elevation_hr_np, \
     perf_eff_np
 
 
-def benchmark(tcx_file_path):
-    print(f"Wczytywanie pliku TCX: {tcx_file_path}")
-    tcx_reader = TCXReader()
-    tcx = tcx_reader.read(tcx_file_path)
+def benchmark(files, output_file="../benchmark.json"):
+    results = []
 
-    points_list = []
-    for trackpoint in tcx.trackpoints:
-        lat = trackpoint.latitude if trackpoint.latitude else 0.0
-        lon = trackpoint.longitude if trackpoint.longitude else 0.0
-        if lat != 0.0 and lon != 0.0:
-            ele = trackpoint.elevation if trackpoint.elevation else 0.0
-            hr = trackpoint.hr_value if trackpoint.hr_value else 0.0
-            cadence = trackpoint.cadence if trackpoint.cadence else 0.0
-            points_list.append((lat, lon, ele, hr, cadence))
+    for file in files:
+        print(f"Przetwarzanie pliku: {file}...")
+        tcx_reader = TCXReader()
+        tcx = tcx_reader.read(file)
 
-    print(f"Liczba punktów do analizy: {len(points_list)}\n")
+        points_list = []
+        for trackpoint in tcx.trackpoints:
+            lat = trackpoint.latitude if trackpoint.latitude else 0.0
+            lon = trackpoint.longitude if trackpoint.longitude else 0.0
+            if lat != 0.0 and lon != 0.0:
+                ele = trackpoint.elevation if trackpoint.elevation else 0.0
+                hr = trackpoint.hr_value if trackpoint.hr_value else 0.0
+                cadence = trackpoint.cadence if trackpoint.cadence else 0.0
+                points_list.append((lat, lon, ele, hr, cadence))
 
-    points_arr = np.array(points_list)
+        points = len(points_list)
+        points_arr = np.array(points_list)
 
-    start = time.perf_counter()
-    bbox_p = bbox(points_list)
-    time_bbox_p = time.perf_counter() - start
+        start_p = time.perf_counter()
+        bbox(points_list)
+        total_distance(points_list)
+        elevation_gain(points_list)
+        avg_hr(points_list)
+        hr_zones(points_list)
+        elevation_hr(points_list)
+        perf_eff(points_list)
+        time_p = time.perf_counter() - start_p
 
-    start = time.perf_counter()
-    bbox_n = bbox_np(points_arr)
-    time_bbox_n = time.perf_counter() - start
+        start_n = time.perf_counter()
+        bbox_np(points_arr)
+        total_distance_np(points_arr)
+        elevation_gain_np(points_arr)
+        avg_hr_np(points_arr)
+        hr_zones_np(points_arr)
+        elevation_hr_np(points_arr)
+        perf_eff_np(points_arr)
+        time_n = time.perf_counter() - start_n
 
-    start = time.perf_counter()
-    dist_p = total_distance(points_list)
-    time_dist_p = time.perf_counter() - start
+        results.append({
+            "plik": file,
+            "punkty": points,
+            "czas_python": time_p,
+            "czas_numpy": time_n,
+            "przyspieszenie": time_p/time_n
+        })
 
-    start = time.perf_counter()
-    dist_n = total_distance_np(points_arr)
-    time_dist_n = time.perf_counter() - start
+    with open(output_file, 'w', encoding='utf-8') as f:
+        json.dump(results, f, indent=4)
 
-    start = time.perf_counter()
-    ele_gain_p = elevation_gain(points_list)
-    time_ele_gain_p = time.perf_counter() - start
-
-    start = time.perf_counter()
-    ele_gain_n = elevation_gain_np(points_arr)
-    time_ele_gain_n = time.perf_counter() - start
-
-    start = time.perf_counter()
-    hr_p = avg_hr(points_list)
-    time_hr_p = time.perf_counter() - start
-
-    start = time.perf_counter()
-    hr_n = avg_hr_np(points_arr)
-    time_hr_n = time.perf_counter() - start
-
-    start = time.perf_counter()
-    zones_p = hr_zones(points_list, hr_max=185)
-    time_zones_p = time.perf_counter() - start
-
-    start = time.perf_counter()
-    zones_n = hr_zones_np(points_arr, hr_max=185)
-    time_zones_n = time.perf_counter() - start
-
-    start = time.perf_counter()
-    ele_hr_p = elevation_hr(points_list)
-    time_ele_hr_p = time.perf_counter() - start
-
-    start = time.perf_counter()
-    ele_hr_n = elevation_hr_np(points_arr)
-    time_ele_hr_n = time.perf_counter() - start
-
-    start = time.perf_counter()
-    perf_eff_p = perf_eff(points_list)
-    time_perf_eff_p = time.perf_counter() - start
-
-    start = time.perf_counter()
-    perf_eff_n = perf_eff_np(points_arr)
-    time_perf_eff_n = time.perf_counter() - start
-
-    print("Wyniki analizy:")
-    print(f"Dystans: {dist_n / 1000:.2f} km | Przewyższenia: {ele_gain_n:.0f} m | Średnie tętno: {hr_n:.0f} BPM")
-    print(f"Czas w strefach HR (Z1-Z5): {zones_n}\n")
-
-    print("| Operacja | Czas Python (s) | Czas NumPy (s) | Przyspieszenie (Py/Np) |")
-    print(f"| Bounding Box | {time_bbox_p:.6f} | {time_bbox_n:.6f} | {time_bbox_p / time_bbox_n:.2f}x |")
-    print(f"| Dystans | {time_dist_p:.6f} | {time_dist_n:.6f} | {time_dist_p / time_dist_n:.2f}x |")
-    print(f"| Przewyższenia | {time_ele_gain_p:.6f} | {time_ele_gain_n:.6f} | {time_ele_gain_p / time_ele_gain_n:.2f}x |")
-    print(f"| Średnie tętno | {time_hr_p:.6f} | {time_hr_n:.6f} | {time_hr_p / time_hr_n:.2f}x |")
-    print(f"| HR Zones | {time_zones_p:.6f} | {time_zones_n:.6f} | {time_zones_p / time_zones_n:.2f}x |")
-    print(f"| Średnie tętno na podjazdach | {time_ele_hr_p:.6f} | {time_ele_hr_n:.6f} | {time_ele_hr_p / time_ele_hr_n:.2f}x |")
-    print(f"| Perfomance Efficiency | {time_perf_eff_p:.6f} | {time_perf_eff_n:.6f} | {time_perf_eff_p / time_perf_eff_n:.2f}x |")
+    print(f"\nGotowe! Zapisano wyniki do pliku: {output_file}")
 
 
 if __name__ == "__main__":
-    benchmark("../data/plik_1000000.tcx")
+    files = [
+        "../data/plik_1000.tcx",
+        "../data/plik_10000.tcx",
+        "../data/plik_100000.tcx",
+        "../data/plik_500000.tcx",
+        "../data/plik_1000000.tcx",
+        "../data/plik_10000000.tcx",
+    ]
+    benchmark(files)
--- scripts/plot.py
+++ scripts/plot.py
@@ -1,79 +1,32 @@
-from tcxreader.tcxreader import TCXReader
-import numpy as np
-import time
+import json
 import matplotlib.pyplot as plt
-from tcx_python import bbox, total_distance, elevation_gain, avg_hr, hr_zones, elevation_hr, perf_eff
-from tcx_numpy import bbox_np, total_distance_np, elevation_gain_np, avg_hr_np, hr_zones_np, elevation_hr_np, perf_eff_np
-
-def parser(tcx_file_path):
-    tcx_reader = TCXReader()
-    tcx = tcx_reader.read(tcx_file_path)
-    points_list = []
-    for trackpoint in tcx.trackpoints:
-        lat = trackpoint.latitude if trackpoint.latitude else 0.0
-        lon = trackpoint.longitude if trackpoint.longitude else 0.0
-        if lat != 0.0 and lon != 0.0:
-            ele = trackpoint.elevation if trackpoint.elevation else 0.0
-            hr = trackpoint.hr_value if trackpoint.hr_value else 0.0
-            cadence = trackpoint.cadence if trackpoint.cadence else 0.0
-            points_list.append((lat, lon, ele, hr, cadence))
-    points = len(points_list)
-    points_arr = np.array(points_list)
-
-    start_p = time.perf_counter()
-    bbox(points_list)
-    total_distance(points_list)
-    elevation_gain(points_list)
-    avg_hr(points_list)
-    hr_zones(points_list)
-    elevation_hr(points_list)
-    perf_eff(points_list)
-    time_p = time.perf_counter() - start_p
-
-    start_n = time.perf_counter()
-    bbox_np(points_arr)
-    total_distance_np(points_arr)
-    elevation_gain_np(points_arr)
-    avg_hr_np(points_arr)
-    hr_zones_np(points_arr)
-    elevation_hr_np(points_arr)
-    perf_eff_np(points_arr)
-    time_n = time.perf_counter() - start_n
-
-    return points, time_p, time_n
 
 
-def plot(files):
-    results = []
-    for file in files:
-        points, time_py, time_np = parser(file)
-        results.append((points, time_py, time_np))
-    results.sort(key=lambda x: x[0])
-    sizes = [r[0] for r in results]
-    python_times = [r[1] for r in results]
-    numpy_times = [r[2] for r in results]
+def plot(file):
+    with open(file, 'r', encoding='utf-8') as f:
+        results = json.load(f)
+
+    results.sort(key=lambda x: x["punkty"])
+
+    sizes = [r["punkty"] for r in results]
+    python_times = [r["przyspieszenie"] for r in results]
+    #numpy_times = [r["czas_numpy"] for r in results]
 
     plt.figure(figsize=(10, 6))
-    plt.plot(sizes, python_times, marker='o', linestyle='-', color='#e74c3c', linewidth=2.5, label='Python')
-    plt.plot(sizes, numpy_times, marker='s', linestyle='-', color='#2980b9', linewidth=2.5, label='NumPy')
-    plt.title('Porównanie czasu analizy na plikach TCX', fontsize=14, pad=15)
+    plt.plot(sizes, python_times, marker='o', linestyle='-', color='#e74c3c', linewidth=2.5, label='Przyspieszenie')
+    #plt.plot(sizes, numpy_times, marker='s', linestyle='-', color='#2980b9', linewidth=2.5, label='NumPy')
+    plt.title('Porównanie przyspieszenia między NumPy a Pythonem na plikach TCX', fontsize=14, pad=15)
     plt.xlabel('Liczba punktów w pliku TCX', fontsize=12)
-    plt.ylabel('Czas wykonania operacji (s)', fontsize=12)
+    plt.ylabel('Przyspieszenie', fontsize=12)
     plt.xscale('log')
     plt.xticks(sizes, [f"{s:,}".replace(',', ' ') for s in sizes])
     plt.grid(True, which="both", linestyle='--', alpha=0.6)
     plt.legend(fontsize=12, loc='upper left')
     plt.tight_layout()
 
-    plt.savefig('../wykres.png', dpi=300)
+    plt.savefig('../wykres2.png', dpi=300)
     plt.show()
 
+
 if __name__ == "__main__":
-    files = [
-        "../data/plik_1000.tcx",
-        "../data/plik_10000.tcx",
-        "../data/plik_100000.tcx",
-        "../data/plik_500000.tcx",
-        "../data/plik_1000000.tcx"
-    ]
-    plot(files)
+    plot("benchmark.json")
