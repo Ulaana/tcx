ad727889fca1276cb8f0fe9ed24c5dc1e759a070
Ulaana <157713810+Ulaana@users.noreply.github.com>
2026-06-10 19:37:52 +0200

    Add files via upload

--- helping_scripts/generate.py
+++ helping_scripts/generate.py
@@ -3,7 +3,7 @@ import math
 import random
 
 
-def generate(filename="../data/plik_1000000.tcx", num_points=100_000):
+def generate(filename="../data/plik_3000000.tcx", num_points=100_000):
     start_time = datetime.datetime.now(datetime.timezone.utc)
     center_lat = 52.0
     center_lon = 19.0
@@ -69,4 +69,4 @@ def generate(filename="../data/plik_1000000.tcx", num_points=100_000):
 
 
 if __name__ == "__main__":
-    generate(num_points=1000000)
+    generate(num_points=3000000)
